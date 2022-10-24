36742da05767311f4de7fafb86abd1bed5c3d54c
Zehedina Smajlovic <usatransport16@gmail.com>
2022-10-24 14:57:33 -0600

    commit tree 88a7a621ce9dd842be2853e5c5a52190bd369002

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat (Ralph){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat (Bob){
+    cats.unshift("Bob");
+    return cats;
+}
+function destructivelyRemoveLastCat (Bob){
+    cats.pop(3);
+    return cats;
+}
+function destructivelyRemoveFirstCat (Milo){
+    cats.shift(0);
+    return cats;
+}
+function appendCat(){
+    const copyCats= [...cats, "Broom"];
+    return copyCats;
+}
+
+function removeLastCat(){
+    const newArray3 = cats.slice(0,-1);
+    return newArray3;
+}
+function prependCat(){
+    const prependCat =["Arnold", ...cats];
+    return prependCat;
+}
+function removeFirstCat(){
+    const removeFirstCat= cats.slice(1);
+    return removeFirstCat;
+    
+}
+
